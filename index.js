55517fe6a407c188f76f7d7bfde50c8690533e3d
SAMIKSHYA-Panda <pandasamikshya313@gmail.com>
2024-07-24 10:42:13 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,4 @@
+document.getElementById('menu-icon').addEventListener('click', function() {
+    var navLinks = document.getElementById('nav-links');
+    navLinks.style.display = navLinks.style.display === 'flex' ? 'none' : 'flex';
+});
